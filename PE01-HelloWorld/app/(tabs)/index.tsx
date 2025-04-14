53c603c64e962ce25cb944f021994a852376118f
Ritish <maniritish2422@gmail.com>
2025-04-14 06:22:40 +0000

    Submission PE01

---
+++ PE01-HelloWorld/app/(tabs)/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>U Sai Mani Ritish</Text>
+      <Text style={styles.text}>MSCS</Text>
+      <Text style={styles.text}>City University of Seattle</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'yellow',
+  },
+  text: {
+    fontSize: 20,
+    margin: 10,
+  },
+});
