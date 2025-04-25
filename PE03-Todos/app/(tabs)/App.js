f45eb4db75129bb4a3ce5fa3a03c4032667d39c1
Ritish <maniritish2422@gmail.com>
2025-04-25 20:36:25 +0000

    Submission

--- PE03-Todos/app/(tabs)/App.js
+++ PE03-Todos/app/(tabs)/App.js
@@ -1,38 +1,62 @@
 import React, { Component } from 'react';
-import { ScrollView, View, StyleSheet } from 'react-native';
-import Input from './Input';
+import { ScrollView, View, StyleSheet, Button, Text } from 'react-native';
 import Heading from './Heading';
+import Input from './Input';
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       inputValue: '',
       todos: [],
-      type: 'All'
     };
   }
 
-  inputChange = (inputValue) => {
-    console.log( ' Input Value: ', inputValue);
-    this.setState({ inputValue : Text});
-  }
+  inputChange = (value) => {
+    this.setState({ inputValue: value });
+  };
+
+  submitTodo = () => {
+    const { inputValue, todos } = this.state;
+    if (inputValue.trim()) {
+      const newTodo = {
+        title: inputValue,
+        completed: false,
+      };
+      this.setState({
+        todos: [...todos, newTodo],
+        inputValue: '',
+      });
+      console.log('Added:', newTodo.title);
+    }
+  };
 
   render() {
-    const{inputValue} =this.state
+    const { inputValue, todos } = this.state;
+
     return (
       <View style={styles.container}>
-        <ScrollView keyboardShouldPersistTaps= "always" contentContainerStyle={styles.content}>
+        <ScrollView
+          keyboardShouldPersistTaps="always"
+          contentContainerStyle={styles.content}
+        >
           <Heading />
-          <Input 
-            inputValue={inputValue}
-            inputChange={text=>this.inputChange(text)}
-          />
+          <Input inputValue={inputValue} inputChange={this.inputChange} />
+          <Button title="Add" onPress={this.submitTodo} />
+
+          <View style={styles.todoList}>
+            {todos.map((todo, index) => (
+              <Text key={index} style={styles.todoItem}>
+                {todo.title}
+              </Text>
+            ))}
+          </View>
         </ScrollView>
       </View>
     );
   }
 }
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
