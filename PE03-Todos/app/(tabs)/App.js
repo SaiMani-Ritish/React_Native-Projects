b537a7663c1c87f17d3998c5905d696680b7fd68
Ritish <maniritish2422@gmail.com>
2025-06-09 03:59:39 +0000

    HOS09

--- PE03-Todos/app/(tabs)/App.js
+++ PE03-Todos/app/(tabs)/App.js
@@ -21,6 +21,20 @@ class App extends Component {
     this.deleteTodo = this.deleteTodo.bind(this)
   }
 
+  componentDidMount() {
+    this.fetchTodos();
+  }
+  
+  fetchTodos = async () => {
+    try {
+      const response = await fetch('https://cityutodoapi.azurewebsites.net/todos');
+      const todos = await response.json();
+      this.setState({ todos });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   submitTodo() {
     if (this.state.inputValue.match(/^\s*$/)) {
       return
